a48299a2876ed74df4eb14a11cbdfcf7422469b3
Abdullah Arshad <57131829+abdullaharshadd@users.noreply.github.com>
2026-07-22 04:02:32 +0500

    chore: add javascript project scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// TODO: mount migrated page routes here
+// import HomePage from './pages/HomePage';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
